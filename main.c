d437c0cc1e2d714bc4dc8d90899c84062ed00f32
utkarsh kashyap <utkarshkashyap1225@gmail.com>
2025-02-23 10:02:06 +0530

    convert ASCII to PACKED_BCD and vice versa

--- main.c
+++ main.c
@@ -4,10 +4,17 @@ void main()
 {
 	
 	char packed_bcd=0x47;
+	char higher=0x34;
+	char lower=0x37;
 	struct ascii as;
 	as=converttoascii(packed_bcd);
 	
 	P1=as.high_byte;
 	P2=as.low_byte;
+	
+	
+	P0=converttobcd(higher, lower);
+	
+	
 	
 }
